670f30b501367a8efef90af30b93e869657acb6b
KasiaBM <kbsadowska@gmail.com>
2016-07-08 15:58:36 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// load express library into variable
+var express = require("express");
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
